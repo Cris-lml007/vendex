0dff70e00f18008cb88bced884b90a6e10585b98
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2026-04-29 14:35:14 -0400

    modal actualizado
    añadido el argumento class

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="{{ $id }}" tabindex="-1" aria-labelledby="{{ $id }}Label" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog {{ $class ?? '' }}">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">{{ $title ?? '' }}</h1>
