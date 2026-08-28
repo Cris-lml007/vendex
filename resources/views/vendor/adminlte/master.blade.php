df05b2b549069359e5b4fe692e6e409a3ded5c67
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2026-08-28 03:38:23 -0400

    temas

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -99,7 +99,7 @@
 
 </head>
 
-<body class="@yield('classes_body')" @yield('body_data')>
+<body class="@yield('classes_body')" @yield('body_data') data-theme="vendex">
 
     {{-- Body Content --}}
     @yield('body')
