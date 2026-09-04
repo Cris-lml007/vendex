9c3946c1b19fe983dcc7016c73dfa50b16d44fb4
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2026-09-04 11:26:47 -0400

    fix

--- database/migrations/tenant/2026_09_03_130254_add_column_tags_to_settings_table.php
+++ database/migrations/tenant/2026_09_03_130254_add_column_tags_to_settings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('settings', function (Blueprint $table) {
-            $table->json('product_tags')->default('[]');
+            $table->json('product_tags')->nullable()->default('[]');
         });
     }
 
